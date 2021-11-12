7b991126c47b83a00188d7e0b3e42744ba4b8b0c
Genetryy <ahlmarvin3@gmail.com>
2021-11-12 23:04:07 +0100

    Update register.php

--- public/views/register.php
+++ public/views/register.php
@@ -89,7 +89,7 @@ $pw2 = $_POST['password_2']
 if($pw1 = $pw2) {
     $pdo = new PDO('mysql:host=localhost;dbname=accounts', 'genetry.net', 'cheA15xeAqxeT4D2Rlyktm^oj');
     // the password param is recognised as a part of the MySQL command. 
-    $statement = $pdo->prepare("INSERT INTO users");
+    $statement = $pdo->prepare("INSERT INTO users VALUES (?, ?)");
     $statement->execute(array($username, $pw1));   
 } else {
     echo "The passwords aren't matching."
