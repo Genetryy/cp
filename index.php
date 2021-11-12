22e5ebefa638bbfc933ece96f95822ae39b4630a
Genetryy <ahlmarvin3@gmail.com>
2021-11-12 21:22:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,26 +6,29 @@
 //
 ///////////////////////////////////////////////////////////
 
+$files = "views";
+$assets = "assets";
+
 $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/' :
-        require __DIR__ . '/views/index.php';
+        require __DIR__ . '/'.$files.'/index.php';
         break;
     case '' :
-        require __DIR__ . '/views/index.php';
+        require __DIR__ . '/'.$files.'/index.php';
         break;
     case '/reset' :
-        require __DIR__ . '/views/passwordrequest.php';
+        require __DIR__ . '/'.$files.'/passwordrequest.php';
         break;
     case '/login' :
-        require __DIR__ . '/views/login.php';
+        require __DIR__ . '/'.$files.'/login.php';
         break;
     case '/styles/login' :
-        require __DIR__ . '/assets/css/login.css';
+        require __DIR__ . '/'.$files.'/css/login.css';
         break;
     default:
         http_response_code(404);
-        require __DIR__ . '/views/404.php';
+        require __DIR__ . '/'.$files.'/404.php';
         break;
 }
