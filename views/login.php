492374389d211299dcd30f7667d5268e28174833
Genetryy <ahlmarvin3@gmail.com>
2021-11-12 21:46:32 +0100

    Update login.php

--- views/login.php
+++ views/login.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="/styles/css/bootstrap">
 </head>
 <body>
-
+    <div class="container-sm">
     <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon1">@</span>
         <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
@@ -14,4 +14,6 @@
         <input type="password" class="form-control" id="exampleFormControlInput1">
     </div>
 
+    <button type="button" class="btn btn-primary">Login</button>
+    </div>
 </body>
