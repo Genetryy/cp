3a243ca4e96d4d821a980dcfebb5da64ff1a9d9a
Genetryy <ahlmarvin3@gmail.com>
2021-11-12 21:04:47 +0100

    +

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
-echo "Hello World!";
-echo "I wanna grab me a beer!";
+
+///////////////////////////////////////////////////////////
+//
+//          Router
+//
+///////////////////////////////////////////////////////////
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/reset' :
+        require __DIR__ . '/views/passwordrequest.php';
+        break;
+    case '/login' :
+        require __DIR__ . '/views/login.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
