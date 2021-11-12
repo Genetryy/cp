566857bccce05046493a57812452d9fe0f012977
Genetryy <ahlmarvin3@gmail.com>
2021-11-12 21:43:45 +0100

    Update login.php

--- views/login.php
+++ views/login.php
@@ -3,12 +3,15 @@
     <link rel="stylesheet" href="/styles/css/bootstrap">
 </head>
 <body>
-    <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
     </div>
+
     <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <label for="exampleFormControlInput1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleFormControlInput1">
     </div>
+
 </body>
