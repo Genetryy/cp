fc90568366ed56873c53c80392ff4bab92b431ae
Genetryy <ahlmarvin3@gmail.com>
2021-11-13 11:56:39 +0100

    Update notify.php

--- private/notify.php
+++ private/notify.php
@@ -3,23 +3,28 @@
 <?php
 
 function notify($type, $msg) {
-    case 'success' :
-        echo "<div class='notify'><div class='alert alert-success' role='alert'>".$msg."</div></div>";
-        break;
-    case 'danger' :
-        echo "<div class='notify'><div class='alert alert-danger' role='alert'>".$msg."</div></div>";
-        break;
-    case 'primary' :
-        echo "<div class='notify'><div class='alert alert-primary' role='alert'>".$msg."</div></div>";
-        break;
-    case 'warning' :
-        echo "<div class='notify'><div class='alert alert-warning' role='alert'>".$msg."</div></div>";
-        break;
-    case 'info' :
-        echo "<div class='notify'><div class='alert alert-info' role='alert'>".$msg."</div></div>";
-        break;
-    default :
-        echo "Please specify a certain type!";
-};
+    switch($type) {
+
+        case 'success' :
+            echo "<div class='notify'><div class='alert alert-success' role='alert'>".$msg."</div></div>";
+            break;
+        case 'danger' :
+            echo "<div class='notify'><div class='alert alert-danger' role='alert'>".$msg."</div></div>";
+            break;
+        case 'primary' :
+            echo "<div class='notify'><div class='alert alert-primary' role='alert'>".$msg."</div></div>";
+            break;
+        case 'warning' :
+            echo "<div class='notify'><div class='alert alert-warning' role='alert'>".$msg."</div></div>";
+            break;
+        case 'info' :
+            echo "<div class='notify'><div class='alert alert-info' role='alert'>".$msg."</div></div>";
+            break;
+        default :
+            echo "Please specify a certain type!";
+            
+    }
+
+}
 
 ?>
