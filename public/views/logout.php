1cf6f38990113415cf1a2a0bb7e3faa3d1107b04
Genetryy <ahlmarvin3@gmail.com>
2021-11-13 10:58:08 +0100

    Update logout.php

--- public/views/logout.php
+++ public/views/logout.php
@@ -1,4 +1,4 @@
-<link href="/styles/bootstrap/css">
+<link href="/styles/css/bootstrap">
 
 <div class="alert alert-danger" role="alert">
   Danger! Do you really want to logout? <a href="/logout">Yes </a>
